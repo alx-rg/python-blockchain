fda8426fd29c167affd26fdc108e7e6dc75470a7
Alex Ross <alxrg@me.com>
2023-01-26 19:47:32 -0600

    defining last_block

--- blockchain.py
+++ blockchain.py
@@ -3,14 +3,33 @@ class Blockchain:
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+    self.new_block(previous_hash=1, proof=100) # genesis block
 
-  def new_block(self):
+  def new_block(self, proof, previous_hash=None):
     # create a new block and add it to the chain
-    pass
-    
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'proof': proof,
+      previous_hash: previous_hash or self.hash(self.chain[-1]),
+    }
+    self.current_transactions = []
+    self.chain.append(block)
+    return block
+  
+
   def new_transaction(self):
     # adds a new transaction to the list of transactions
-    pass
+    # create new transaction, send to the next block. 
+    # transaction contains var. sender, recipient and amount
+    self.current_transactions.append(
+      {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount,
+      }
+    )
+    return self.last_block['index'] + 1
 
   def register_node(self):
     # add a new node to the list of nodes
@@ -34,6 +53,6 @@ class Blockchain:
   @property
 
   def last_block(self):
-    # returns the last block in the chain
-    pass
+    # returns the last block in the chain 
+    return self.chain[-1]
   
